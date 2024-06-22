f47d9f638271c7c58c057e685427f61d31c1064f
Abhishek Rawat <abr07k@gmail.com:git config --global user.email abr07k@gmail.com>
2024-06-22 11:40:41 +0530

    file system node module

--- Node/2.2 Native Modules/index.js
+++ Node/2.2 Native Modules/index.js
@@ -4,13 +4,14 @@ const fs = require("fs"); //importing file system module
 // "callback if error throw error other console log that file has been saved"
 // cd karke terminal me path waha ka daal le .... jaha pe dalna hai
 
-fs.writeFile("message.txt","Abhishek Rawat" , (err) => {
-    if (err) throw err;
-    console.log("the file has been saved!");
-});
-
-//read the file msg.txt
-// const fs = require('fs');
+// fs.writeFile("message.txt","Abhishek Rawat" , (err) => {
+//     if (err) throw err;
+//     console.log("the file has been saved!");
+// });
+// fs.writeFile("new.txt","i added something/nothing to this file" , (err) => {
+//     if (err) throw err;
+//     console.log("the file has been saved!");
+// });
 
 // fs.readFile("message.txt","utf-8" ,(err, data) => {
 //   if (err) {
@@ -19,3 +20,11 @@ fs.writeFile("message.txt","Abhishek Rawat" , (err) => {
 //   }
 //   console.log(data);
 // });
+
+fs.readFile("new.txt","utf-8" ,(err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
